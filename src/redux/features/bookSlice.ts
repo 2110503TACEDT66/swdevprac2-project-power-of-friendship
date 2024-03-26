570d0982e15f15adc30032f3bf3dba7594496747
Tanboon Tiengtut <tanboon.top3837@gmail.com>
2024-03-26 21:32:33 +0700

    comment redux

--- src/redux/features/bookSlice.ts
+++ src/redux/features/bookSlice.ts
@@ -1,30 +1,30 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-type BookState =  {
-    bookItems: BookingItem[]
-}
+// type BookState =  {
+//     bookItems: BookingItem[]
+// }
 
-const initialState: BookState = { bookItems:[] }
+// const initialState: BookState = { bookItems:[] }
 
-export const bookSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addBooking: (state, action:PayloadAction<BookingItem>) => {
-            const checkingId = state.bookItems.filter(obj => {
-                return ((obj.id !== action.payload.id))
-            })
-            checkingId.push(action.payload)
-            state.bookItems = checkingId
-        },
-        removeBooking: (state, action:PayloadAction<string>) => {
-            const remainItems = state.bookItems.filter( obj => {
-                return ((obj.id !== action.payload))
-            })
-            state.bookItems = remainItems
-        }
-    }
-})
+// export const bookSlice = createSlice({
+//     name: "cart",
+//     initialState,
+//     reducers: {
+//         addBooking: (state, action:PayloadAction<BookingItem>) => {
+//             const checkingId = state.bookItems.filter(obj => {
+//                 return ((obj.id !== action.payload.id))
+//             })
+//             checkingId.push(action.payload)
+//             state.bookItems = checkingId
+//         },
+//         removeBooking: (state, action:PayloadAction<string>) => {
+//             const remainItems = state.bookItems.filter( obj => {
+//                 return ((obj.id !== action.payload))
+//             })
+//             state.bookItems = remainItems
+//         }
+//     }
+// })
 
-export const { addBooking, removeBooking } = bookSlice.actions
-export default bookSlice.reducer
+// export const { addBooking, removeBooking } = bookSlice.actions
+// export default bookSlice.reducer
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,23 +1,23 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import bookSlice from "./features/bookSlice";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { persistReducer } from "redux-persist"
-import storage from "redux-persist/lib/storage"
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import bookSlice from "./features/bookSlice";
+// import { TypedUseSelectorHook, useSelector } from "react-redux";
+// import { persistReducer } from "redux-persist"
+// import storage from "redux-persist/lib/storage"
 
-const persistConfig = {
-    key: "rootPersist",
-    storage
-}
+// const persistConfig = {
+//     key: "rootPersist",
+//     storage
+// }
 
-const rootReducer = combineReducers({bookSlice})
-const reduxPersistedReducer = persistReducer(persistConfig, rootReducer)
+// const rootReducer = combineReducers({bookSlice})
+// const reduxPersistedReducer = persistReducer(persistConfig, rootReducer)
 
 
-export const store = configureStore({
+// export const store = configureStore({
         
-        reducer: reduxPersistedReducer
-})
+//         reducer: reduxPersistedReducer
+// })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+// export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
